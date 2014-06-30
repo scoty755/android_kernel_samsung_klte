c25eea384826c5327b736941d298ad7efdd0cce8
Matt Filetto <matt.filetto@gmail.com>
2014-06-30 04:43:20 -0700

    add initial chronic-config.sh for boot-up options (WIP)
    Change-Id: I17972b5dc21025bf3da3e392788587d12bfcf793
    
    Conflicts:
    	Packages/chronic-config.sh

--- Packages/chronic-config.sh
+++ Packages/chronic-config.sh
@@ -15,52 +15,34 @@
 
 # Possible values
 
-# GOV=smartmax
+# GOV+smartmax
+# GOV=smartassH3
 # GOV=linoheart
 # GOV=badass
 # GOV=wheatley
 # GOV=userspace
 # GOV=consevative
 # GOV=intelliactive
+# GOV=lagfee
 # GOV=performance
 # GOV=ondemand
 GOV=interactive
 
 # Config CPU frequency
 # Default: 2457600 (2.45Ghtz)
-# available frequencies:
-# 300000 345600 422400 499200 576000 652800 729600 806400 883200 960000 1036800 1113600 1190400 1267200 1344000 1420800
-# 1497600 1574400 1651200 1728000 1804800 1881600 1958400 2035200 2112000 2150400 2188800 2265600 2342400 2419200 2457600
-# 2534400 2611200 2688000 2764800 2841600 2899200
+# Range: 300000 - 2899200 (300Mhtz - 2.89Ghtz)
 #
 # Max/Min
 MAXFREQ=2457600
 MINFREQ=300000
 
 
-# Config GPU clock Max Frequency
+# Config 3D GPU clock
 # 389000000 = 389mhz (Super Underclocked)
 # 462400000 = 450mhz (Underclocked)
 # 578000000 = 578mhz (Default)
 # 657500000 = 657mhz (Performance)
-GPU_MAX=578000000
-
-
-# Config GPU clock Min Frequency
-
-# ** Choose either Samsung default 320Mhtz or Standard 200Mhtz
-# ** key is as follows..
-
-# 320Mhtz = 4
-# 200Mhtz = 5
-GPU_MIN=4
-
-
-# I/O Scheduler
-# 
-# Available schedulers: cfq, bfq, fiops, noop, deadline, row, sio, vr, zen, fifo
-# Device default: cfq
-SCHED=cfq
+GPU=578000000
 
 
 # Enable Fastcharge
@@ -69,13 +51,6 @@ SCHED=cfq
 # 2 = substitute AC to USB charging only if there is no USB peripheral detected
 FASTCHARGE=0
 
-
-# Chronic Hotplug
-# set custom "enable_core_thresholds" This is represented as four numbers each being for the corosponding core (0,1,2.3)
-# the numbers are derived from cpu load times 10
-# 0 = always on
-# 999 = never on
-ENABLE_CORE_THRES="0 290 340 390"
 
 
 # End of configurable options #
@@ -116,18 +91,9 @@ ENABLE_CORE_THRES="0 290 340 390"
 
 
 # Config 3D GPU setting
-    echo $GPU_MAX > /sys/class/kgsl/kgsl-3d0/max_gpuclk
-    echo $GPU_MIN > /sys/class/kgsl/kgsl-3d0/min_pwrlevel
+    echo $GPU > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 
 
 # Config USB forced fastcharge
     echo "$FASTCHARGE" > /sys/kernel/fast_charge/force_fast_charge
-
-
-# Config I/O sched
-    echo $SCHED > /sys/block/mmcblk0/queue/scheduler
-
-
-# Config Chronic Hotplug
-    echo $ENABLE_CORE_THRES > /sys/devices/system/cpu/cpufreq/chronic_hotplug/enable_core_thresholds
 
