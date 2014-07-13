1e02fc84fc891cae67ce057de0dc76f573102080
Matt Filetto <matt.filetto@gmail.com>
2014-07-13 11:16:16 -0700

    chronic-hotplug: move all the defines to the header file
    Change-Id: I11fb495007176f804f88cf22141c49e2ca2568fd

--- arch/arm/kernel/chronic_hotplug.h
+++ arch/arm/kernel/chronic_hotplug.h
@@ -20,3 +20,61 @@
 #include <linux/earlysuspend.h>
 #endif
 
+/*
+ * Enable debug output to dump the average
+ * calculations and ring buffer array values
+ * WARNING: Enabling this causes a ton of overhead
+ *
+ * FIXME: Turn it into debugfs stats (somehow)
+ * because currently it is a sack of shit.
+ */
+#define DEBUG 0
+
+#define CPUS_AVAILABLE		num_possible_cpus()
+/*
+ * SAMPLING_PERIODS * MIN_SAMPLING_RATE is the minimum
+ * load history which will be averaged
+ */
+#define SAMPLING_PERIODS	15
+#define INDEX_MAX_VALUE		(SAMPLING_PERIODS - 1)
+/*
+ * MIN_SAMPLING_RATE is scaled based on num_online_cpus()
+ */
+#define MIN_SAMPLING_RATE	msecs_to_jiffies(20)
+
+/*
+ * Load defines:
+ * ENABLE_ALL is a high watermark to rapidly online all CPUs
+ *
+ * ENABLE is the load which is required to enable 1 extra CPU
+ * DISABLE is the load at which a CPU is disabled
+ * These two are scaled based on num_online_cpus()
+ */
+#define ENABLE_ALL_LOAD_THRESHOLD	440
+#define ENABLE_LOAD_THRESHOLD		290
+#define DISABLE_LOAD_THRESHOLD		250
+static int enable_load[] = { 0, 290, 340, 390 };
+static int hotplug_cpu_single_on[] = { 0, 0, 0, 0 };
+static int hotplug_cpu_single_off[] = { 0, 0, 0, 0 };
+/* Control flags */
+unsigned char flags;
+#define HOTPLUG_DISABLED	(1 << 0)
+#define HOTPLUG_PAUSED		(1 << 1)
+#define BOOSTPULSE_ACTIVE	(1 << 2)
+#define EARLYSUSPEND_ACTIVE	(1 << 3)
+
+struct delayed_work hotplug_decision_work;
+struct delayed_work hotplug_unpause_work;
+struct work_struct hotplug_online_all_work;
+struct work_struct hotplug_online_single_work;
+struct delayed_work aphotplug_offline_work;
+struct work_struct hotplug_offline_all_work;
+struct work_struct hotplug_boost_online_work;
+
+static unsigned int history[SAMPLING_PERIODS];
+static unsigned int index;
+
+static unsigned int min_online_cpus = 2;
+
+static bool isEnabled = true;
+static bool EnableOverride = false;
